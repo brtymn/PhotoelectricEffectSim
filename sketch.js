731b8478fb2fdc147fb7086e99270a345b5cd8a4
Bartu Yaman <brtymn@protonmail.com>
2020-09-20 15:28:46 +0300

    Updated.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 // UI definitions start.
 let W = 1200  //width of bgCanvas
 let H = 500  //height of bgCanvas
-let Wsim = W * 0.69
+let Wsim = W * 0.69 
 let Hsim = H
 let Wplot = 0.25 * W
-let Hplot = 0.875 * H
+let Hplot = 0.875 * H 
 let bgCanvas, simCanvas, plotCanvas;
 // UI definitions end.
 
-
+ 
 // Constant definitions start.
 
 let plancks_constant = (6.626 * Math.pow(10, -34)); // In Joules * Seconds.
@@ -27,16 +27,14 @@ let light_wavelength = 0; // In nanometers.
 
 // Photoemissive material work function definitions start.
 
-let surface_Al_Ti_V = 4.3 // Aluminum, Titanium, Vanadium
-let surface_Cr = 4.5; // Chromium
-let surface_Mn = 4.1; // Manganese
-let surface_Fe = 4.7; // Iron
-let surface_Co = 5; // Cobalt
-let surface_Ni = 5.15; // Nickel
-let surface_W = 4.55; // Tungsten
-let surface_Au = 5.1; // Gold
+let surface_Al = 4.3 // Aluminum Surface
+let surface_Cr = 4.5; // Chromium Surface
+let surface_Mn = 4.1; // Manganese Surface
+let surface_Fe = 4.7; // Iron Surface
+let surface_Co = 5; // Cobalt Surface
+let surface_Au = 5.1; // Gold Surface
 
-let selected_work_function = 0; // Will add a selection mechanism.
+let selected_surface = 0; // Will add a selection mechanism.
 
 // Photoemissive material work function definitions end.
 
@@ -44,9 +42,9 @@ let selected_work_function = 0; // Will add a selection mechanism.
 
 function setup() {
   bgCanvas = createCanvas(W, H)
-
+  
   simCanvas = createGraphics(Wsim, Hsim)
-
+  
   plotCanvas = createGraphics(Wplot, Hplot)
   plotCanvas.background(20)
   plotCanvas.stroke(255)
@@ -62,23 +60,57 @@ function setup() {
   simCanvas.strokeWeight(2)
   simCanvas.noFill()
   simCanvas.rect(10, 10, Wsim - 20, Hsim - 20)
-
+  simCanvas.fill(255);
+  simCanvas.arc(400, 150, 300, 25, PI, TWO_PI);
+  
   //sim canvas
   image(simCanvas, 0, 0);
-
-}
+   
+// Selection using the dropdown fucntions will be added here.
+  if(selected_surface = surface_Al){
+     selected_surface = surface_Al;
+     rect(250, 350, 300, 25);
+     fill('white');
+     }
+  else if(selected_surface == surface_Cr) {
+    selected_surface = surface_Cr;
+    rect(250, 350, 300, 25);
+    fill('white');
+    }
+  else if(selected_surface == surface_Mn) {
+    selected_surface = surface_Mn;
+    rect(250, 350, 300, 25);
+    fill('white');
+    }
+  else if(selected_surface == surface_Fe) {
+    selected_surface = surface_Fe;
+    rect(250, 350, 300, 25);
+    fill('white');
+    }
+  else if(selected_surface == surface_Co) {
+    selected_surface = surface_Co;
+    rect(250, 350, 300, 25);
+    fill('white');
+    }
+  else if(selected_surface == surface_Au) {
+    selected_surface = surface_Au;
+    rect(250, 350, 300, 25);
+    fill('white');
+    }
+  
+} 
 
 // Convert wavelength values to frequency.
 function wavelength_to_frequency(){
-
+  
   return (1 / light_wavelength);
-
+  
 }
 
 
 // Calculate energy for each wave packet.
 function calculate_energy(){
-
+  
   let light_frequency = wavelength_to_frequency();
 
   return (plancks_constant * light_frequency);
@@ -90,7 +122,7 @@ function calculate_energy(){
 function calculate_max_kinetic_energy(){
 
   let packet_energy = calculate_energy();
-
+  
   return (packet_energy - selected_work_function);
 
 }
@@ -99,6 +131,9 @@ function calculate_max_kinetic_energy(){
 // Calculate the cutoff wavelength.
 function calculate_cutoff_wavelength(){
 
-  return ((plancks_constant * speed_of_light) / (selected_work_function))
+  return ((plancks_constant * speed_of_light) / (selected_surface))
 
 }
+
+
+
