11f9822c191fa1c59463bd7e87f3fea0d54dcc3a
Bartu Yaman <brtymn@protonmail.com>
2020-09-21 13:37:48 +0300

    Added the "Electron" class

---
+++ electron.js
@@ -0,0 +1,13 @@
+
+class Electron {
+  constructor(input_x, input_y, input_vx, input_vy, input_colorr, input_diameter) {
+
+    this.position_vector = createVector(input_x, input_y);
+    this.velocity_vector = createVector(input_vx, input_vy);
+    
+    this.diameter = input_diameter; // Diameter definition, may be changed to a slider later on.
+    
+    this.colorr = input_colorr; // Color definition.
+    
+  }  
+}
